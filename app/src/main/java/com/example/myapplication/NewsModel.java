6cb257bdafa85de5741279737c9c79c8bbea5f2f
Aref Azizian <storm.aref@outlook.com>
2019-06-14 01:55:58 +0430

    list view

---
+++ app/src/main/java/com/example/myapplication/NewsModel.java
@@ -0,0 +1,11 @@
+package com.example.myapplication;
+
+public class NewsModel {
+    String title;
+    String text;
+
+    public NewsModel(String title, String text) {
+        this.title = title;
+        this.text = text;
+    }
+}
--- app/src/main/java/com/example/myapplication/news.java
+++
@@ -1,7 +0,0 @@
-package com.example.myapplication;
-
-import android.support.v7.app.AppCompatActivity;
-
-public class news extends AppCompatActivity {
-
-}
